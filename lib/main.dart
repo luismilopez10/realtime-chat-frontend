4f59ee48c0ba3e51fd485f4d37f8df5aef067d02
Luis Miguel Lopez <luis.lopezl@sofka.com.co>
2024-08-18 20:37:49 -0500

    Add flutter localizations (en, es)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'package:provider/provider.dart';
@@ -36,6 +37,15 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Chat App',
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('es', 'ES'),
+        ],
         theme: ThemeData(
           // colorScheme: ColorScheme.fromSeed(
           //     seedColor: AppColors.instance.sendMessageColor),
