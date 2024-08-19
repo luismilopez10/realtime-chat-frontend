162fbc48e03853b08defcfec7ff5b8ccfd2ffce7
Luis Miguel Lopez <luis.lopezl@sofka.com.co>
2024-08-18 20:39:17 -0500

    Update localhost connection for Android devices and emulators

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 class Environment {
   static final String apiUrl = Platform.isAndroid
-      ? 'http://10.0.2.2:3000/api'
+      // ? 'http://10.0.2.2:3000/api' //* Para emuladores Android
+      ? 'http://192.168.1.8:3000/api' //* Para dispositivos físicos (la ip puede variar en cada conexión)
       : 'http://localhost:3000/api';
 
-  static final String socketUrl =
-      Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+  static final String socketUrl = Platform.isAndroid
+      // ? 'http://10.0.2.2:3000' //* Para emuladores Android
+      ? 'http://192.168.1.8:3000' //* Para dispositivos físicos (la ip puede variar en cada conexión)
+      : 'http://localhost:3000';
 }
